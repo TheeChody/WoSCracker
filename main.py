849eb1697e48840dacd93e5129243defeccf0cc1
Cody <chodywhooch@live.ca>
2025-02-08 03:46:46 -0700

    Update main.py

--- main.py
+++ main.py
@@ -4,22 +4,49 @@ import time
 from timeit import default_timer as timer
 
 if getattr(sys, 'frozen', False):
-    application_path = f"{os.path.dirname(sys.executable)}\\_internal"
+    application_path = f"{os.path.dirname(sys.executable)}\\_internal\\"
 else:
-    application_path = os.path.dirname(__file__)
+    application_path = f"{os.path.dirname(__file__)}\\"
 
-letter_values = {'a': 1, 'b': 3, 'c': 3, 'd': 2, 'e': 1, 'f': 4, 'g': 2, 'h': 4, 'i': 1, 'j': 0, 'k': 5, 'l': 1, 'm': 3,
-                 'n': 1, 'o': 1, 'p': 3, 'q': 10, 'r': 1, 's': 1, 't': 1, 'u': 1, 'v': 4, 'w': 4, 'x': 8, 'y': 4, 'z': 10}
+letter_values = {'a': 1,
+                 'b': 3,
+                 'c': 3,
+                 'd': 2,
+                 'e': 1,
+                 'f': 4,
+                 'g': 2,
+                 'h': 4,
+                 'i': 1,
+                 'j': 8,
+                 'k': 5,
+                 'l': 1,
+                 'm': 3,
+                 'n': 1,
+                 'o': 1,
+                 'p': 3,
+                 'q': 10,
+                 'r': 1,
+                 's': 1,
+                 't': 1,
+                 'u': 1,
+                 'v': 4,
+                 'w': 4,
+                 'x': 8,
+                 'y': 4,
+                 'z': 10}
+
+data_path = f"{application_path}Data\\"
+
+
+def cls():
+    os.system('cls' if os.name == 'nt' else 'clear')
 
 
 def word_check(letter_split: list, hidden: bool):
+    cls()
     start = timer()
     valid_words = {}
     possible_words = {}
-
-    with open(f"{application_path}\\ref\\english_91k", "r") as file:
-        words = file.read()
-        words_list = list(map(str, words.splitlines()))
 
     for word in words_list:
         correct_letter = 0
@@ -53,37 +80,68 @@ def word_check(letter_split: list, hidden: bool):
 def run():
     while True:
         try:
-            fake = input("Fake Letter; Y/N\n")
-            if fake.lower() in ("y", "yes"):
+            fake = input("Fake Letter; Y/N\n").lower()
+            cls()
+            if fake in ("y", "yes"):
                 fake = True
             else:
                 fake = False
 
-            hidden = input("Hidden Letter; Y/N\n")
-            if hidden.lower() in ("y", "yes"):
+            hidden = input("Hidden Letter; Y/N\n").lower()
+            cls()
+            if hidden in ("y", "yes"):
                 hidden = True
             else:
                 hidden = False
 
-            letters = input("Enter letters\n")
+            letters = input("Enter letters\n").lower()
             letter_split = list(letters)
             word_check(letter_split, hidden)
             if fake:
-                letter_remove = input("Input Fake Letter\n")
+                letter_remove = input("Input Fake Letter\n").lower()
                 try:
                     letter_split.remove(letter_remove)
                 except Exception as f:
                     print(f)
                     break
                 word_check(letter_split, hidden)
+                while True:
+                    right_fake = input("Right Fake Letter? Y/N\n").lower()
+                    if right_fake in ("n", "no"):
+                        next_fake = input("Input Fake Letter\n")
+                        try:
+                            letter_split.append(letter_remove)
+                            letter_split.remove(next_fake)
+                        except Exception as f:
+                            print(f)
+                            break
+                        letter_remove = next_fake
+                        word_check(letter_split, hidden)
+                    else:
+                        break
             if hidden:
-                letter_add = input("Input Hidden Letter\n")
+                letter_add = input("Input Hidden Letter\n").lower()
                 try:
                     letter_split.append(letter_add)
                 except Exception as f:
                     print(f)
                     break
                 word_check(letter_split, False)
+                while True:
+                    right_hidden = input("Right Hidden Letter? Y/N\n").lower()
+                    if right_hidden in ("n", "no"):
+                        next_hidden = input("Input Hidden Letter\n").lower()
+                        try:
+                            letter_split.remove(letter_add)
+                            letter_split.append(next_hidden)
+                        except Exception as f:
+                            print(f)
+                            break
+                        letter_add = next_hidden
+                        word_check(letter_split, False)
+                    else:
+                        break
+            input("Press Enter For Next Round"), cls()
         except KeyboardInterrupt:
             print("Exiting program")
             break
@@ -95,4 +153,11 @@ def run():
 
 
 if __name__ == "__main__":
+    try:
+        with open(f"{data_path}english_91k", "r") as file:
+            words = file.read()
+            words_list = list(map(str, words.splitlines()))
+    except FileNotFoundError:
+        print(f"Word List Not Found!\nApp Path; {application_path}\nData Path; {data_path}\nFile Name; english_91k\nAll Together; {data_path}english_91k\nProgram Will Close In 90 Sec, Or Manually Close"), time.sleep(90)
+        os._exit(1)
     run()
